1a373634ff637945939b5cede9b0bbfe5f50902f
Juan Ospina <jeol3739@gmail.com>
2023-06-08 16:13:12 -0500

    Ejercicio Extra 13

---
+++ src/ejercicioextra13/EjercicioExtra13.java
@@ -0,0 +1,27 @@
+/*
+Crear un programa que dibuje una escalera de números, donde cada línea de números
+comience en uno y termine en el número de la línea. Solicitar la altura de la escalera al
+usuario al comenzar. Ejemplo: si se ingresa el número 3:
+1
+12
+123
+ */
+package ejercicioextra13;
+
+import java.util.Scanner;
+
+public class EjercicioExtra13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingrese la altura de la escalera: ");
+        int altura = scanner.nextInt();
+        
+        for (int i = 1; i <= altura; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
